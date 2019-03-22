c93175c9dd30b7319579577587375f0790d38688
Sarah Tuitoek <sjtoek@gmail.com>
2019-03-22 14:19:51 +0300

    add views.py

--- neighbour/views.py
+++ neighbour/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http  import HttpResponse
 
 # Create your views here.
+def landing(request):
+    return render(request,'landing.html')
+
+def profile(request):
+    return render(request,'profile/profile.html')
+
+def editdp(request):
+    return render(request,'profile/editdp.html')  
